9250ea787bfbdf950c34cfa19c3c658b4502d3f5
Gordoco <gordoco416@gmail.com>
2024-04-21 01:14:08 -0500

    Update PlayerMove.cs

--- Assets/Scripts/Player/Controls/PlayerMove.cs
+++ Assets/Scripts/Player/Controls/PlayerMove.cs
@@ -47,7 +47,7 @@ public class PlayerMove : NetworkBehaviour
 
     void Update()
     {
-        if (!isOwned || bDead || !bAwake) return;
+        if (!isOwned || bDead || !bAwake || !isClient) return;
         CharacterController controller = GetComponent<CharacterController>();
         StatManager manager = GetComponent<StatManager>();
         if (controller.isGrounded)
