56a6125123777201b3f12a0ff1a7823dbb940d09
Gordoco <gordoco416@gmail.com>
2024-04-21 17:49:18 -0500

    Stopped Stupid Era

--- Assets/Scripts/Player/Controls/ClassAttacks/CommandoAttack.cs
+++ Assets/Scripts/Player/Controls/ClassAttacks/CommandoAttack.cs
@@ -131,17 +131,18 @@ public class CommandoAttack : AttackManager
         Debug.Log((float)manager.GetStat(NumericalStats.JumpHeight));
 
         if (JetpackParticleEffect != null) JetpackParticleEffect.SetActive(true);
-        if (isClient) GetComponent<PlayerMove>().SetFlying(true);
+        GetComponent<PlayerMove>().SetFlying(true);
         while (count < 50)
         {
-            if (isClient && controller.enabled) controller.Move(Vector3.up * (float)manager.GetStat(NumericalStats.JumpHeight) * 5 * Time.deltaTime);
+            if (controller.enabled) controller.Move(Vector3.up * (float)manager.GetStat(NumericalStats.JumpHeight) * 5 * Time.deltaTime);
             count++;
             yield return new WaitForSeconds(0.02f);
         }
         count = 0;
         if (JetpackParticleEffect != null) JetpackParticleEffect.SetActive(false);
-        if (isClient) GetComponent<PlayerMove>().SetFlying(false);
+        GetComponent<PlayerMove>().SetFlying(false);
     }
+
     protected override void OnAbility4()
     {
 
--- Assets/Scripts/Player/Controls/PlayerMove.cs
+++ Assets/Scripts/Player/Controls/PlayerMove.cs
@@ -47,7 +47,8 @@ public class PlayerMove : NetworkBehaviour
 
     void Update()
     {
-        if (!isOwned || bDead || !bAwake || !isClient) return;
+        if (!isOwned || bDead || !bAwake) return;
+
         CharacterController controller = GetComponent<CharacterController>();
         if (!controller.enabled) return;
         StatManager manager = GetComponent<StatManager>();
