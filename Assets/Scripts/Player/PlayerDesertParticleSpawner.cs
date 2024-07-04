617f12b1a8c0bde5291e3d458d4cd0324815a17e
Gordoco <gordoco416@gmail.com>
2024-07-04 15:02:08 -0500

    More Level Design Prog

---
+++ Assets/Scripts/Player/PlayerDesertParticleSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDesertParticleSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject ParticlePrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ParticlePrefab) return;
+        GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
+        if (Players.Length > 0)
+        {
+            for (int i = 0; i < Players.Length; i++)
+            {
+                if (Players[i].transform.parent.name == "LocalGamePlayer")
+                {
+                    GameObject particle = Instantiate(ParticlePrefab);
+                    particle.transform.parent = Players[i].transform;
+                    particle.transform.position = Players[i].transform.position;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
