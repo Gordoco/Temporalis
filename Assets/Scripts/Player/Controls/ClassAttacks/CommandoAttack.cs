3492f53a97ea6c289f37f76d8e150adc3aa8c135
Cody Gordon <gordoco416@gmail.com>
2024-10-26 14:49:41 -0500

    Update CommandoAttack.cs

--- Assets/Scripts/Player/Controls/ClassAttacks/CommandoAttack.cs
+++ Assets/Scripts/Player/Controls/ClassAttacks/CommandoAttack.cs
@@ -75,8 +75,8 @@ public class CommandoAttack : AttackManager
         GameObject MF2 = Instantiate(PrimaryAttackParticleEffect, Gun2MuzzleLoc, Quaternion.LookRotation(dir));
         PlayShootSound();
 
-        LShotStart.GetComponent<ParticleSystem>().Play();
-        RShotStart.GetComponent<ParticleSystem>().Play();
+        if (gameObject.transform.root.name == "LocalGamePlayer") LShotStart.GetComponent<ParticleSystem>().Play();
+        if (gameObject.transform.root.name == "LocalGamePlayer") RShotStart.GetComponent<ParticleSystem>().Play();
 
         if (Physics.Raycast(start1, dir, out hit1, (float)statManager.GetStat(NumericalStats.Range))) { /*Debug.Log("GUN 1 HIT OBJECT");*/ }
         if (Physics.Raycast(start2, dir, out hit2, (float)statManager.GetStat(NumericalStats.Range))) { /*Debug.Log("GUN 2 HIT OBJECT");*/ }
